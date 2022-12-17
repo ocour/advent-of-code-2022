785ad648ceebfc688b800bd2b98482040576a9d5
ocour <112856256+ocour@users.noreply.github.com>
2022-12-17 18:33:51 +0200

    Update main.c

--- day3_c/main.c
+++ day3_c/main.c
@@ -7,8 +7,7 @@
 #include "part2.h"
 
 /* 
-	For some reason this program gives access error for compartment2, other times it does not, i dont know why, 
-	might have fixed with (i + half), rather than just i + half;
+	For some reason this program gives access error for compartment2, other times it does not, i dont know why
 */
 /* Also program is slow, need ways to improve it */
 
